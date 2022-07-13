e6c8506b5a0b7f9b6d62dc2a2046b059722818ce
viktor <viktor.bulkin@stremline-c.com>
2022-07-13 17:40:17 +0300

    add filters

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,24 @@ import $ from './lib/lib';
 
 // $('.active').on('click', sayHello);
 $('button').on('click', function () {
-    $(this).toggleClass('active');
+    $('div').eq(4).toggleClass('active');
 });
 
 
+$('div').click(function () {
+    console.log($(this).index());
+
+})
+
+// console.log($('button').html('hi'));
+
+
+
+// console.log($("div").eq(2).find('.more'))
+// console.log($('div').eq(1))
+
+
+// console.log($('.some').closest('.findMe'))
+
+
+console.log($('.more').eq(0).siblings());
