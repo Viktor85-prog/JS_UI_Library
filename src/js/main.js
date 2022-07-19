d8bf8ad9e507f5884dccc408825164efd23b450f
viktor <viktor.bulkin@stremline-c.com>
2022-07-19 14:14:34 +0300

    add dropdown menu

--- src/js/main.js
+++ src/js/main.js
@@ -13,3 +13,19 @@ $('[data-count="second"]').on('click', () => {
 $('button').eq(2).on('click', () => {
     $('.w-500').fadeToggle(800);
 })
+
+
+$('.wrap').html(
+    `<div class="dropdown">
+            <button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton">Drop btn</button>
+
+            <div class="dropdown-menu" data-toggle-id="dropdownMenuButton">
+                <a href="#" class="dropdown-item">wrfwrfwr</a>
+                <a href="#" class="dropdown-item">wrfwrfwr</a>
+                <a href="#" class="dropdown-item">wrfwrfwr</a>
+            </div>
+
+        </div>`
+);
+
+$('.dropdown-toggle').dropdown();
